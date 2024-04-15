2998d5ee211dcc34d9139c50516c17ce1a059caf
Jan Hamal Dvořák <mordae@anilinux.org>
2024-04-15 10:33:41 +0200

    ili9341: configure gamma

--- ili9341.c
+++ ili9341.c
@@ -32,6 +32,24 @@ void tft_preflight(void)
 	tft_control(0x11, NULL, 0);
 	sleep_ms(120);
 
+	/* 8.2.17 GAMSET: Gamma Set */
+	uint8_t gamset[] = { 1 };
+	tft_control(0x26, gamset, sizeof gamset);
+
+	/* 8.3.24 PGAMCTRL: Positive Gamma Correction (from Linux) */
+	uint8_t pgamctrl[] = { 0x0f, 0x31, 0x2b, 0x0c, 0x0e, 0x08, 0x4e, 0xf1,
+			       0x37, 0x07, 0x10, 0x03, 0x0e, 0x09, 0x00 };
+	tft_control(0xe0, pgamctrl, sizeof pgamctrl);
+
+	/* 8.3.25 NGAMCTRL: Negative Gamma Correction (from Linux) */
+	uint8_t ngamctrl[] = { 0x00, 0x0e, 0x14, 0x03, 0x11, 0x07, 0x31, 0xc1,
+			       0x48, 0x08, 0x0f, 0x0c, 0x31, 0x36, 0x0f };
+	tft_control(0xe1, ngamctrl, sizeof ngamctrl);
+
+	/* 8.4.7 Enable_3G: Enable 3G */
+	uint8_t enable3g[] = { 0 };
+	tft_control(0xf2, enable3g, sizeof enable3g);
+
 	/* 8.2.6 MADCTL: Memory Access Data Control */
 	uint8_t madctl = (0 << 5) | (TFT_FLIP_X << 6) | (TFT_FLIP_Y << 7) |
 			 (1 << 3) /* BGR swap */ | (TFT_FLIP_Y << 4) | (TFT_FLIP_X << 2);
